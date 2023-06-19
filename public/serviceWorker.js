0b421bf4a3c01e746bc644191cc7113c592017c2
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-06-19 23:39:01 +0330

    Create service worker & Config pwa functionality

---
+++ public/serviceWorker.js
@@ -0,0 +1,42 @@
+const CACHE = "weather-app-v-0.0.1";
+const urlToCache = ["index.html", "offline.html"];
+const self = this;
+
+// Install SW
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlToCache);
+    })
+  );
+});
+
+// Listen for request
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(() => {
+      return fetch(e.request).catch(() => caches.match("offline.html"));
+    })
+  );
+});
+
+// Activate the SW
+
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE);
+
+  e.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
