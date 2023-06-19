0f957ae4686d7c2c99665f641c27f4387a468605
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2023-06-19 18:30:16 +0330

    Create loading component

---
+++ src/components/Loading.js
@@ -0,0 +1,14 @@
+// ant design
+import { LoadingOutlined } from "@ant-design/icons";
+// Components
+import Container from "./Container";
+
+const Loading = () => {
+  return (
+    <Container>
+      <LoadingOutlined />
+    </Container>
+  );
+};
+
+export default Loading;
